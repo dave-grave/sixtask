61fbe8739ad7f586794675769d6981600c8ff98b
David Huang <djhuang636@gmail.com>
2025-03-26 18:42:03 -0400

    import tailwind and hello world

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <div>
+      <h1 className="text-3xl font-bold underline">hello world</h1>
+    </div>
+  </StrictMode>
+);
