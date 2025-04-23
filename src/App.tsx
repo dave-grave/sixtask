9b1d01f4c0ca35fbba5d8bc31a4ac6e34b360d13
David Huang <djhuang636@gmail.com>
2025-04-22 21:09:38 -0400

    fix post accesstoken in useAuth.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,32 @@
-import { useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  Navigate,
-} from "react-router-dom";
-import TaskPage from "./components/TaskPage";
-import TimerSelector from "./components/TimeSelector";
-import Callback from "./components/Callback";
-import SpotifyPlayerPage from "./components/SpotifyPlayerPage";
-import Login from "./components/Login";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  const [token, setToken] = useState<string | null>(
-    localStorage.getItem("spotifyAccessToken")
-  );
+  // const [token, setToken] = useState<string | null>(
+  //   localStorage.getItem("spotifyAccessToken")
+  // );
 
-  useEffect(() => {
-    const checkToken = () => {
-      const storedToken = localStorage.getItem("spotifyAccessToken");
-      setToken(storedToken);
-    };
+  // useEffect(() => {
+  //   const checkToken = () => {
+  //     const storedToken = localStorage.getItem("spotifyAccessToken");
+  //     setToken(storedToken);
+  //   };
 
-    checkToken();
+  //   checkToken();
 
-    window.addEventListener("storage", checkToken);
+  //   window.addEventListener("storage", checkToken);
 
-    return () => {
-      window.removeEventListener("storage", checkToken);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("storage", checkToken);
+  //   };
+  // }, []);
 
-  return (
-    <Router>
-      <div className="app">
-        <nav>
-          <Link to="/">Tasks</Link>
-          <Link to="/timer">Timer</Link>
-          <Link to="/spotify">spotify</Link>
-          <Link to="/login">login</Link>
-          {token && (
-            <span className="ml-4 text-green-400"> Spotify Connected</span>
-          )}
-        </nav>
-        <Routes>
-          <Route path="/" element={<TaskPage />} />
-          <Route path="/timer" element={<TimerSelector />} />
-          <Route path="/callback" element={<Callback />} />
-          <Route
-            path="/spotify"
-            element={token ? <SpotifyPlayerPage /> : <Navigate to="/login" />}
-          />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  const code = new URLSearchParams(window.location.search).get("code");
+
+  return code ? <Dashboard code={code} /> : <Login />;
+  // return <Login />;
 }
 
 export default App;
---
+++ src/Appcopy.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import TaskPage from "./components/TaskPage";
+import TimerSelector from "./components/TimeSelector";
+import Callback from "./components/Callback";
+import SpotifyPlayerPage from "./components/SpotifyPlayerPage";
+import Login from "./Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("spotifyAccessToken")
+  );
+
+  useEffect(() => {
+    const checkToken = () => {
+      const storedToken = localStorage.getItem("spotifyAccessToken");
+      setToken(storedToken);
+    };
+
+    checkToken();
+
+    window.addEventListener("storage", checkToken);
+
+    return () => {
+      window.removeEventListener("storage", checkToken);
+    };
+  }, []);
+
+  return (
+    <Router>
+      <div className="app">
+        <nav>
+          <Link to="/">Tasks</Link>
+          <Link to="/timer">Timer</Link>
+          <Link to="/spotify">spotify</Link>
+          <Link to="/login">login</Link>
+          {token && (
+            <span className="ml-4 text-green-400"> Spotify Connected</span>
+          )}
+        </nav>
+        <Routes>
+          <Route path="/" element={<TaskPage />} />
+          <Route path="/timer" element={<TimerSelector />} />
+          <Route path="/callback" element={<Callback />} />
+          <Route
+            path="/spotify"
+            element={token ? <SpotifyPlayerPage /> : <Navigate to="/login" />}
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
