feb7dfa9c998dbd171b0c284dab0ed995e0b4acf
David Huang <djhuang636@gmail.com>
2025-04-25 16:28:48 -0400

    fix refreshtoken post error

--- client/src/useAuth.tsx
+++ client/src/useAuth.tsx
@@ -2,47 +2,59 @@ import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 export default function useAuth(code: string) {
-  const [accessToken, setAccessToken] = useState();
-  const [refreshToken, setRefreshToken] = useState();
-  const [expiresIn, setExpiresIn] = useState();
-  const hasRun = useRef(false);
+  const [accessToken, setAccessToken] = useState<string>();
+  const [refreshToken, setRefreshToken] = useState<string>();
+  const [expiresIn, setExpiresIn] = useState<string>();
+
+  const loginHasRun = useRef(false);
+  const refreshHasRun = useRef(false);
 
   useEffect(() => {
-    if (hasRun.current) return;
-    hasRun.current = true;
+    if (loginHasRun.current) return;
+    loginHasRun.current = true;
 
     axios
       .post("http://localhost:3001/login", {
         code,
       })
       .then((res) => {
-        console.log(res.data);
-        // setAccessToken(res.data.accessToken);
-        // setRefreshToken(res.data.refreshToken);
-        // setExpiresIn(res.data.expiresIn);
-        // window.history.pushState({}, null, "/");
+        console.log("/login access token", res.data);
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, "", "/");
       })
       .catch((err) => {
-        console.log(err);
-        // window.location.href = "/";
+        console.log("/login POST error", err);
+        window.location.href = "/";
       });
   }, [code]);
 
-  //   useEffect(() => {
-  //     axios
-  //       .post("http://localhost:3001/refresh", {
-  //         refreshToken,
-  //       })
-  //       .then((res) => {
-  //         // setAccessToken(res.data.accessToken);
-  //         // setRefreshToken(res.data.refreshToken);
-  //         // setExpiresIn(res.data.expiresIn);
-  //         // window.history.pushState({}, null, "/");
-  //       })
-  //       .catch(() => {
-  //         window.location.href = "/";
-  //       });
-  //   }, [refreshToken, expiresIn]);
+  useEffect(() => {
+    if (refreshHasRun.current) return;
+    refreshHasRun.current = true;
 
-  //   return accessToken;
+    if (!refreshToken) {
+      console.log("no refresh token yet");
+      return;
+    }
+
+    axios
+      .post("http://localhost:3001/refresh", {
+        refreshToken,
+      })
+      .then((res) => {
+        console.log("/refresh access token", res.data);
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        // window.history.pushState({}, "", "/");
+      })
+      .catch((err) => {
+        console.log("/refresh POST error", err);
+        // window.location.href = "/";
+      });
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
 }
