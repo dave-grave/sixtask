85b8d831e83af607d900e939fe0f38cacd4e2926
David Huang <djhuang636@gmail.com>
2025-04-26 17:22:25 -0400

    delete comments in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,25 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  // const [token, setToken] = useState<string | null>(
-  //   localStorage.getItem("spotifyAccessToken")
-  // );
-
-  // useEffect(() => {
-  //   const checkToken = () => {
-  //     const storedToken = localStorage.getItem("spotifyAccessToken");
-  //     setToken(storedToken);
-  //   };
-
-  //   checkToken();
-
-  //   window.addEventListener("storage", checkToken);
-
-  //   return () => {
-  //     window.removeEventListener("storage", checkToken);
-  //   };
-  // }, []);
-
   const code = new URLSearchParams(window.location.search).get("code");
 
   return code ? <Dashboard code={code} /> : <Login />;
