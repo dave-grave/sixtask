2a2be6a9bd4435334b997a4605e09cb96036be16
David Huang <djhuang636@gmail.com>
2025-05-05 13:47:37 -0400

    style fileexplorer.tsx

--- client/src/FileExplorer.tsx
+++ client/src/FileExplorer.tsx
@@ -47,11 +47,20 @@ export default function FileExplorer() {
   };
 
   return (
-    <div className=" flex flex-col items-center">
-      <input type="file" onChange={handleFileChange}></input>
+    <div className="py-8 flex flex-col items-center h-screen justify-between">
+      <div className="flex-grow flex items-center justify-center">
+        <input
+          className="block w-full text-sm text-gray-500 
+          file:mr-4 file:py-2 file:px-4 file:rounded file:border-solid 
+          file:text-sm file:font-semibold file:bg-gray-200 
+          file:text-blue-700 hover:file:bg-gray-500 file:transition file:duration-300 "
+          type="file"
+          onChange={handleFileChange}
+        ></input>
+      </div>
 
       {file && (
-        <div className="mb-4 text-sm">
+        <div className="mb-4 text-sm flex-grow flex flex-col items-center justify-center">
           <p>File name: {file.name}</p>
           <p>Size: {(file.size / 1024).toFixed(2)} KB</p>
           <p>Type: {file.type}</p>
@@ -59,30 +68,38 @@ export default function FileExplorer() {
       )}
 
       {file && (
-        <button
-          type="button"
-          className={`px-4 py-2 rounded transition ${
-            status === "uploading"
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-500 hover:bg-blue-700 transition px-4 py-2 rounded"
-          }`}
-          onClick={handleFileUpload}
-          disabled={status === "uploading"}
-        >
-          {status === "uploading" ? (
-            <LoadingFileBuffer uploadProgress={uploadProgress} />
-          ) : (
-            "upload a file..."
+        <div className="flex-grow flex-col flex items-center justify-center">
+          <button
+            type="button"
+            className={`px-4 py-2 rounded transition ${
+              status === "uploading"
+                ? "bg-gray-400 cursor-not-allowed"
+                : "bg-blue-500 hover:bg-blue-700 transition px-4 py-2 rounded"
+            }`}
+            onClick={handleFileUpload}
+            disabled={status === "uploading"}
+          >
+            {status === "uploading" ? (
+              <LoadingFileBuffer uploadProgress={uploadProgress} />
+            ) : (
+              "upload a file..."
+            )}
+          </button>
+
+          {status === "success" && (
+            <div>
+              <p className="text-sm text-green-600">
+                file uploaded successfully!
+              </p>
+            </div>
           )}
-        </button>
-      )}
 
-      {status === "success" && (
-        <p className="text-sm text-green-600">file uploaded successfully!</p>
-      )}
-
-      {status === "error" && (
-        <p className="text-sm text-red-600">file upload failed</p>
+          {status === "error" && (
+            <div>
+              <p className="text-sm text-red-600">file upload failed</p>
+            </div>
+          )}
+        </div>
       )}
     </div>
   );
