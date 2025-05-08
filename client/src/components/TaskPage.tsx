58c79bfe30d6e942cc265b35c3ac4f2c765b16d1
David Huang <djhuang636@gmail.com>
2025-05-08 18:44:50 -0400

    fix flex visual bug on taskpage.tsx

--- client/src/components/TaskPage.tsx
+++ client/src/components/TaskPage.tsx
@@ -19,15 +19,15 @@ export default function TaskPage() {
   };
 
   return (
-    <div className="flex flex-col items-center gap-12 py-4">
-      <h1 className="text-xl font-bold mb-4">sixtask</h1>
+    <div className="flex flex-col items-center justify-around py-4 min-h-screen">
+      <h1 className="text-xl font-bold">sixtask</h1>
       {tasks.map((task, index) => (
         <input
           key={index}
           type="text"
           value={task}
           onChange={(e) => handleInputChange(index, e.target.value)}
-          className="w-64 p-2 border boder-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-64 p-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder={`Task ${index + 1}`}
         />
       ))}
