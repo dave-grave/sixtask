6ad1d29a04ce0ca73f6108bba0ff99b898087c14
David Huang <djhuang636@gmail.com>
2025-05-31 11:51:20 -0400

    add shadcn barchart

--- src/app/home/Content.tsx
+++ src/app/home/Content.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Tasks from "../../components/content/Tasks";
 import Timer from "../../components/content/timer/Timer";
 import Spotify from "@/components/content/spotify/Spotify";
-import Dashboard from "@/components/content/Dashboard";
+import Dashboard from "@/components/content/dashboard/Dashboard";
 import { NavPageType } from "@/app/home/page";
 
 export default function Content({ currentPage }: { currentPage: NavPageType }) {
